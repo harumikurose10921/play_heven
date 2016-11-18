9e145bf60743a5c26216de095ebc13cbc6639597
harumi <a>
2016-11-18 10:48:24 +0900

    animation追加 武器持たせた

--- PlayHeven/Assets/script/Enemy.cs
+++ PlayHeven/Assets/script/Enemy.cs
@@ -2,14 +2,27 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
-
+    GameObject player;
+    public float move=0.5f;
 	// Use this for initialization
 	void Start () {
-	
+        player = GameObject.Find("overlord");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        float dist = Vector3.Distance(transform.localPosition, player.transform.localPosition);
+        Vector3 pos = transform.localPosition;
+        pos.x -= move;
+        transform.localPosition = pos;
+        if (dist < 0.5f)
+        {
+            GameObject.Destroy(gameObject);
+        }
+        //CharacterController ctr = GetComponent<CharacterController>();
+        //Vector3 move = new Vector3();
+        //move.x -= 0.05f;
+        //ctr.Move(move);
+        
 	}
 }
--- PlayHeven/Assets/script/Player.cs
+++ PlayHeven/Assets/script/Player.cs
@@ -2,7 +2,22 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-
+    IEnumerator _SetTrigger(string name)
+    {
+        Animator animator = GetComponent<Animator>();
+        animator.SetBool(name, true);
+        yield return new WaitForSeconds(0);
+        animator.SetBool(name, false);
+    }
+    /// <summary>
+    /// フラグのトリガー設定。
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    void SetTrigger(string name)
+    {
+        StartCoroutine(_SetTrigger(name));
+    }
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +25,14 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        Animator animator = GetComponent<Animator>();
+        if (Input.GetKeyDown(KeyCode.A) == true)
+        {
+            SetTrigger("isattack1L");
+        }
+        if (Input.GetKeyDown(KeyCode.D) == true)
+        {
+            SetTrigger("block");
+        }
 	}
 }
