cdabae3d31ab7780f7169fd06f6afe6b387d30d9
doi <a>
2017-02-09 11:04:55 +0900

    pull 敵すぽーん

--- PlayHeven/Assets/script/hit.cs
+++ PlayHeven/Assets/script/hit.cs
@@ -3,16 +3,16 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class hit : MonoBehaviour {
+    GameObject enemy;
+    // Use this for initialization
+    void Start () {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
         
-	}
+    }
 
     void OnTriggerEnter(Collider collision)
     {
@@ -20,18 +20,21 @@ public class hit : MonoBehaviour {
         //animator.speed = -1.0f;
         if (collision.gameObject.tag == "enemy")
         {
-            SceneManager.LoadScene("result");
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(enemy, transform.localPosition, transform.rotation);
+                SceneManager.LoadScene("result");
+            }
+            if (collision.gameObject.tag == "Player")
+            {
+                SceneManager.LoadScene("title");
+            }
+            //for(int i = 0; i < 2; i++)
+            //{
+            //    //クリア条件にする
+            //    Destroy(gameObject);
+            //}
         }
-        if (collision.gameObject.tag == "Player")
-        {
-            SceneManager.LoadScene("title");
-        }
-        //for(int i = 0; i < 2; i++)
-        //{
-        //    //クリア条件にする
-        //    Destroy(gameObject);
-        //}
-       
     }
 
     
--- PlayHeven/Assets/script/result.cs
+++ PlayHeven/Assets/script/result.cs
@@ -10,7 +10,7 @@ public class result : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetKey(KeyCode.A)==true)
+        if (Input.GetKey(KeyCode.A) == true) 
         {
             SceneManager.LoadScene("result");
         }
